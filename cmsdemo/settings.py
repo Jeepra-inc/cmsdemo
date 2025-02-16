43559c64c9c5f462049a18406f8bd63928b2f552
Nishesh Patel <jeepraofficial@gmail.com>
2025-02-16 12:53:19 +0545

    added database

--- cmsdemo/settings.py
+++ cmsdemo/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'cmsdemo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ebdb', 
+        'USER': 'jeepra', 
+        'PASSWORD': 'Jeepra123', 
+        'HOST': 'awseb-e-6mbmzqxmpa-stack-awsebrdsdatabase-ue8ivfwm2uju.cb8miimwiusn.us-west-2.rds.amazonaws.com', 
+        'PORT': '5432',
     }
 }
 
